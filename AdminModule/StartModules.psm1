786c3d1b877a87cf99f818dbebd88b279ca8aa9e
Blah Smith <blah@czadd.com>
2023-03-21 07:40:35 -0700

    broke it

--- AdminModule/StartModules.psm1
+++ AdminModule/StartModules.psm1
@@ -13,31 +13,9 @@ Add-AdminHelp -AliasName vmm -Description "Start Virtual Machine Manager module"
 New-Alias -name sex -Value (Join-path $go.'msol and exchange' 'Start-MSOLconnectQuick.ps1') # -description "StartModules" 
 Add-AdminHelp -AliasName sex -Description "Start Exhange Online session" -Examples "sex"
 
-Function Connect-Compellent{
-    [CmdletBinding()]
-    Param(
-        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true,Position=0)][ValidateSet('Prod','DR')][String]$Environment,
-        [Parameter(Mandatory=$false,ValueFromPipelineByPropertyName=$true,Position=1)][String]$CompellentCredPath = (Join-path $env:APPDATA 'compellentcred.xml')
-    )
-
-    #Add-PSSnapin Compellent.StorageCenter.PSSnapin
-    
-    if( -not (Test-path $CompellentCredPath) ){
-        $CompellentCred = Get-Credential -Message "Compellent Login"
-        $CompellentCred | Export-Clixml -Path $CompellentCredPath
-    }
-    Else{
-        $CompellentCred = Import-Clixml $CompellentCredPath
-    }
-
-    Switch( $Environment ){
-        Prod{ $CompellentHost = '10.10.2.11'  }
-        DR  { $CompellentHost = '10.10.20.2.11'  }
-    }
-
-    $Connection = Get-SCConnection -HostName $CompellentHost -User $CompellentCred.UserName $CompellentCred.Password -Save $Environment -Default
-}
+#OMGibrokeit
 
 export-modulemember -function * -variable * -alias * -Cmdlet * 
 
 #comment
+#comment2
